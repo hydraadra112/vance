cab49fa3271524fb0b4d642221f69bda682f00a5
John Manuel Carado <carado.johnp11q@gmail.com>
2026-02-08 16:46:18 +0800

    refactor(process): Make process as dataclass

--- base.py
+++ base.py
@@ -1,11 +1,7 @@
-class Process():
-    def __init__(self, pid: int, burst_time: int):
-        if not isinstance(pid, int) or not isinstance(burst_time, int):
-            raise TypeError("PID and Burst Time must be integers.")
-        if burst_time < 0:
-            raise ValueError("Burst time cannot be negative.")
-            
-        self.pid = pid
-        self.burst_time = burst_time
-        self.waiting_time = None
-        self.turnaround_time = None
+from dataclasses import dataclass
+@dataclass
+class Process:
+    pid: int
+    burst_time: int
+    waiting_time: int | None = None
+    turnaround_time: int | None = None
