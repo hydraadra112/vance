002a7148c081da46dce8926ddf0f40b51e9cf806
John Manuel Carado <carado.johnp11q@gmail.com>
2026-02-09 22:14:23 +0800

    refactor(fcfs): To utilize ProcessResult

--- src/aevum/schedulers.py
+++ src/aevum/schedulers.py
@@ -1,5 +1,6 @@
 from metrics import calculate_turnaround_time, calculate_waiting_time
-from base import Process
+from base import Process, ProcessResult
+from utils import is_sorted_by_arrival
 
 def run_fcfs_simulation(processes: list[Process]) -> dict:
     """
@@ -14,31 +15,46 @@ def run_fcfs_simulation(processes: list[Process]) -> dict:
     if not processes:
         raise ValueError("Process list is empty!")
 
-    for i in range(len(processes)):
-        current = processes[i]
-        previous = processes[i - 1] if i > 0 else None
-        
-        calculate_waiting_time(current, previous)
-        calculate_turnaround_time(current)
+    # Check if sorted
+    if not is_sorted_by_arrival(processes):
+        raise ValueError(
+            "FCFS requires processes to be sorted by arrival time. "
+            "Please use 'aevum.utils.sort_processes_by_arrival()' before passing the list."
+        )
 
-    avg_wait = sum(p.waiting_time for p in processes) / len(processes)
-    avg_tat = sum(p.turnaround_time for p in processes) / len(processes)
+    process_results: list[ProcessResult] = []
+    current_time = 0
+
+    for process in processes:
+        waiting_time = calculate_waiting_time(process.arrival_time, current_time)
+        turnaround_time = calculate_turnaround_time(process.burst_time, waiting_time)
+        completion_time = process.arrival_time + turnaround_time
+
+        # Append results
+        process_results.append(ProcessResult(
+            process=process,
+            waiting_time=waiting_time,
+            turnaround_time=turnaround_time,
+            completion_time=completion_time
+        ))
+        
+        current_time = completion_time
+
+    avg_wait = sum(r.waiting_time for r in process_results) / len(process_results)
+    avg_tat = sum(r.turnaround_time for r in process_results) / len(process_results)
 
     # Construct Output Dictionary
-    output = {
+    return {
         "individual_results": [
             {
-                "pid": p.pid,
-                "burst": p.burst_time,
-                "wait": p.waiting_time,
-                "turnaround": p.turnaround_time
-            } for p in processes
+                "pid": r.process.pid,
+                "wait": r.waiting_time,
+                "turnaround": r.turnaround_time,
+                "completion": r.completion_time
+            } for r in process_results
         ],
         "averages": {
             "avg_waiting_time": round(avg_wait, 2),
             "avg_turnaround_time": round(avg_tat, 2)
-        },
-        "total_completion_time": processes[-1].turnaround_time
+        }
     }
-
-    return output
